9c150e55586a70ccafc5c8cac1bce25c2ff23f00
strictlyemoji <147510301+strictlyemoji@users.noreply.github.com>
2023-10-12 11:27:29 +0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,12 +1,16 @@
-/* JavaScript to show/hide the popup */
-const lipaButton = document.getElementById('lipa-button');
-const popup = document.getElementById('popup');
-const closePopup = document.getElementById('close-popup');
-
-lipaButton.addEventListener('click', () => {
-  popup.style.display = 'block';
+document.getElementById("lipa-button").addEventListener("click", function() {
+    document.getElementById("popup").style.display = "block";
 });
 
-closePopup.addEventListener('click', () => {
-  popup.style.display = 'none';
+document.getElementById("close-popup").addEventListener("click", function() {
+    document.getElementById("popup").style.display = "none";
+});
+
+document.getElementById("make-payment").addEventListener("click", function() {
+    // Add code here to handle the payment, e.g., sending a request to your server.
+    var phoneNumber = document.getElementById("phone-number").value;
+    var amount = document.getElementById("amount").value;
+    
+    // For now, let's just show the entered values.
+    alert("Phone Number: " + phoneNumber + "\nAmount: " + amount);
 });
